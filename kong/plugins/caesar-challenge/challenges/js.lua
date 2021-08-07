c5cee670946232970ff2d3bc57bab053d6863f36
Rabbit <rq0net@gmail.com>
2021-08-07 12:46:00 +0800

    debug info

--- kong/plugins/caesar-challenge/challenges/js.lua
+++ kong/plugins/caesar-challenge/challenges/js.lua
@@ -14,13 +14,13 @@ local _M = {}
 --- Create new js challange object
 -- @table opts Strategy options: contains 'dictionary_name' and 'ttl' fields
 function _M.new(opts)
-  local self = {
-    opts = opts,
-  }
-
-  return setmetatable(self, {
-    __index = _M,
-  })
+    local self = {
+      opts = opts,
+    }
+  
+    return setmetatable(self, {
+      __index = _M,
+    })
 end
 
 
