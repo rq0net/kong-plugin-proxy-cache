4948162cb365e05ff7263e9794ddf22684b0d8d9
Rabbit <rq0net@gmail.com>
2021-08-07 11:47:32 +0800

    debug info

--- kong/plugins/caesar-challenge/schema.lua
+++ kong/plugins/caesar-challenge/schema.lua
@@ -14,9 +14,42 @@ return {
             type = "string",
             one_of = challenges.CHALLENGE_TYPES,
             required = true,
-          }}
+          }},
+          { js = {
+            type = "record",
+            fields = {
+              { dictionary_name = {
+                type = "string",
+                required = false,
+                default = "kong_db_cache",
+              }},
+            },
+          }},
+          { cookie = {
+            type = "record",
+            fields = {
+              { dictionary_name = {
+                type = "string",
+                required = false,
+                default = "kong_db_cache",
+              }},
+            },
+          }},
+          { puzzle = {
+            type = "record",
+            fields = {
+              { dictionary_name = {
+                type = "string",
+                required = false,
+                default = "kong_db_cache",
+              }},
+            },
+          }},
         },
       },
+
+
+      
     },
   },
 }
