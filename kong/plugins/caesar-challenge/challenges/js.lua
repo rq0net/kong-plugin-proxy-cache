3904bbb82d455b70b64dbaf38f26ae306dd8a4eb
Rabbit <rq0net@gmail.com>
2021-08-07 11:26:53 +0800

    debug info

--- kong/plugins/caesar-challenge/challenges/js.lua
+++ kong/plugins/caesar-challenge/challenges/js.lua
@@ -9,14 +9,27 @@ local setmetatable = setmetatable
 
 local _M = {}
 
-  function _M:challenge()
-    -- ngx.var.testcookie_var = "on"
-    res = {}
-    res.status = anti.authentication_page_status_output
-    res.body = anti.anti_ddos_html_output
-    res.header.content_type = "text/html; charset=" .. anti.default_charset
 
-    return res
-  end
+--- Create new memory strategy object
+-- @table opts Strategy options: contains 'dictionary_name' and 'ttl' fields
+function _M.new(opts)
+  local self = {
+    opts = opts,
+  }
 
-  return _M
+  return setmetatable(self, {
+    __index = _M,
+  })
+end
+
+function _M:challenge()
+  -- ngx.var.testcookie_var = "on"
+  res = {}
+  res.status = anti.authentication_page_status_output
+  res.body = anti.anti_ddos_html_output
+  res.header.content_type = "text/html; charset=" .. anti.default_charset
+
+  return res
+end
+
+return _M
