2e89dc5b693ae1614a91c057b7ba37207858ff43
Rabbit <rq0net@gmail.com>
2021-08-07 12:41:16 +0800

    debug info

--- kong/plugins/caesar-challenge/challenges/cookie.lua
+++ kong/plugins/caesar-challenge/challenges/cookie.lua
@@ -21,7 +21,19 @@ function _M.new(opts)
     return setmetatable(self, {
       __index = _M,
     })
-  end
+end
 
+
+function _M:challenge()
+    -- ngx.var.testcookie_var = "on"
+    res = {}
+    res.status = 200
+    res.body = "hello"
+    res.header.content_type = "text/html"
   
+    return res, nil
+end
+  
+
+
 return _M
