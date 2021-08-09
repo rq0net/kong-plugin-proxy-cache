6fcb669763778ad7323dfa8f7f29fd3fff702f5f
Rabbit <rq0net@gmail.com>
2021-08-09 15:35:38 +0800

    add geo plugin

--- kong/plugins/caesar-geoip/schema.lua
+++ kong/plugins/caesar-geoip/schema.lua
@@ -1,5 +1,4 @@
 local iputils = require "resty.iputils"
-local Errors = require "kong.dao.errors"
 
 local function validate_ips(v, t, column)
   if v and type(v) == "table" then
@@ -16,17 +15,23 @@ end
 return {
   name = "caesar-geoip",
   fields = {
-    whitelist_ips = {type = "array", func = validate_ips},
-    blacklist_countries = {type = "array"}
-  },
-  self_check = function(schema, plugin_t, dao, is_update)
-    local wl = type(plugin_t.whitelist_ips) == "table" and plugin_t.whitelist_ips or {}
-    local bl = type(plugin_t.blacklist_countries) == "table" and plugin_t.blacklist_countries or {}
-
-    if #wl == 0 and #bl == 0 then
-      return false, Errors.schema "you must set at least a whitelist_ips or blacklist_countries"
-    end
-
-    return true
-  end
+    { config = {
+      type = "record",
+      fields = {
+        { whitelist_ips = {
+          type = "array",
+          elements = { type = "string" },
+          required = false,
+          custom_validator = validate_ips
+        }},
+        { request_method = {
+          type = "array",
+          elements = {
+            type = "string"
+          },
+          required = false
+        }},
+      }
+    }},
+  }
 }
